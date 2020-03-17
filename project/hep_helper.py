ff7f6712514945e79c0c73c2e2c0484cd734d860
Szymon Oleksy <soleksy.bs@gmail.com>
2020-03-17 22:42:16 +0100

    restructuring the project , adding basic commands , formating using autopep8

--- project/hep_helper.py
+++ project/hep_helper.py
@@ -3,20 +3,22 @@ import sys
 from urllib.request import urlopen
 from collections import defaultdict
 
-def hep_url_generator(command_string,format,BAI):
-    if format in ['json' , 'out' , None]:
+
+def hep_url_generator(command_string, format, BAI):
+    if format in ['json', 'out', None]:
         format = "recjson"
-    if BAI == True:
-        url = "http://inspirehep.net/search?ln=en&cc=HepNames&p=" + command_string + \
-        "&of=" +format
+    if BAI:
+        url = "http://inspirehep.net/search?ln=en&cc=HepNames&p=" + \
+            command_string + "&of=" + format
     else:
         url = "https://inspirehep.net/search?p=" + command_string + \
-            "&of=" +format+ "&ot=creator,creation_date,title"
-    
+            "&of=" + format + "&ot=creator,creation_date,title"
+
     return url
 
-def save_to_json(url , filename):
-    
+
+def save_to_json(url, filename):
+
     with urlopen(url) as resp:
         source = resp.read()
 
@@ -26,6 +28,7 @@ def save_to_json(url , filename):
         string = json.dumps(data, indent=2)
         f.write(string)
     print("Succesfully saved contents of the query to: " + filename)
+
 
 def display_in_cmd(url):
     with urlopen(url) as resp:
--- project/pyper.py
+++ project/pyper.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import hep_helper
 
+
 def replace_spaces(string):
     return string.replace(" ", "%20")
 
@@ -10,7 +11,7 @@ def main():
     commands = ""
     flag = 0
     BAI = False
-    
+
     parser = argparse.ArgumentParser()
 
     subparsers = parser.add_subparsers(
@@ -54,19 +55,20 @@ def main():
         help="Search for papers published by specific amount of authors for example: '-ac 10+ , -ac 1->10",
         type=str)
 
-    #Add affiliation to the query
+    # Add affiliation to the query
     parser_HEP.add_argument(
         "-aff",
         required=False,
         help="If you want to search for papers published by people affiliated to a particular institution, you can use this operator. Note, however, that you need to know how the institution is registered in INSPIRE’s database",
         type=str)
-    #Wrtie result to the JSON file
+    
+    # Wrtie result to the JSON file
     parser_HEP.add_argument(
         "-save",
         required=False,
-        help="Write contents of the query to the json file" , 
-        type = str,
-        choices = ['json'],
+        help="Write contents of the query to the json file",
+        type=str,
+        choices=['json'],
     )
     # Article based search arguments:
     parser_HEP.add_argument(
@@ -74,6 +76,7 @@ def main():
         required=False,
         help="Adds title of the paper to the query",
         type=str)
+    
     # need to add: TEXKEY , EPRINT, DOI, REPORT_NUMBER , RECORD_ID, DOC_TYPE
     # DATE , JOURNAL ,CITATION NUMBER , CITATION OF A RECORD
 
@@ -84,7 +87,6 @@ def main():
         help="Adds the name/s of the author/s to the query",
         type=str)
 
-    
     args = parser.parse_args()
     hep_args = parser_HEP.parse_args(args=sys.argv[2:])
 
@@ -114,14 +116,16 @@ def main():
         commands += "affiliation:" + replace_spaces(hep_args.ea) + "%20"
 
     if flag == 0:
-        print("Choose at least one query variable try:\n" + "python3 pyper.py HEP -h " + "for more information")
-    else:          
+        print("Choose at least one query variable try:\n" +
+              "python3 pyper.py HEP -h " + "for more information")
+    else:
         if hep_args.save is not None:
-            url = hep_helper.hep_url_generator(commands , hep_args.save,BAI)
-            hep_helper.save_to_json(url ,"sample_api.json")
+            url = hep_helper.hep_url_generator(commands, hep_args.save, BAI)
+            hep_helper.save_to_json(url, "sample_api.json")
         else:
-            url = hep_helper.hep_url_generator(commands , hep_args.save,BAI)
+            url = hep_helper.hep_url_generator(commands, hep_args.save, BAI)
             hep_helper.display_in_cmd(url)
-            
+
+
 if __name__ == "__main__":
     main()
