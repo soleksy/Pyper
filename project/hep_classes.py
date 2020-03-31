9e3b7081b13bec1ce47f89e26dbe8b8530534f72
linuxuser <Szymon.Oleksy@student.uj.edu.pl>
2020-03-31 19:08:32 +0200

    Adding: 1) Parameters now covering 90% of all HEP use cases 2)  Friendly output to the file and command line,  3) Update on documentation 4) Sorting for names,date,citations

--- project/hep_classes.py
+++ project/hep_classes.py
@@ -3,29 +3,31 @@ import sys
 from urllib.request import urlopen
 from collections import defaultdict
 
+
 class Hep_Helper:
 
-    def hep_url_encode(self , string):
+    def hep_url_encode(self, string):
         encode_list = [(" ", "%20"), (":", "%3A"), ("/", "%2" + "F")]
         for el1, el2 in encode_list:
             string = string.replace(el1, el2)
         return string
 
-
     def hep_url_generator(self, command_string, format):
         format = "recjson"
 
-        url = "https://old.inspirehep.net/search?p=" + command_string + \
-            "&of=" + format + "&ot=creator,title,creation_date,number_of_citations"
+        url = "https://old.inspirehep.net/search?p=" + command_string + "&of=" + format + \
+            "&ot=creator,title,creation_date,number_of_citations,primary_report_number,doi"
 
-            #"&ot=creator,creation_date,title,doi,primary_report_number,number_of_citations,reference,recid"
-        
+        # "&ot=creator,creation_date,title,doi,primary_report_number,number_of_citations,reference,recid"
+
         print(url)
         return url
-    
+
     def get_source(self, url):
         with urlopen(url) as resp:
             source = resp.read()
+
+            self.write_to_json(source, "API_OUTPUT_JSON.json")
 
             return source
 
@@ -36,43 +38,47 @@ class Hep_Helper:
         with open(filename, 'w') as f:
             string = json.dumps(data, indent=2)
             f.write(string)
-        print("Succesfully saved contents of the query to: " + filename)
 
-
-    def display_in_cmd(self, url):
-        with urlopen(url) as resp:
-            source = resp.read()
-        data = json.loads(source)
-        string = json.dumps(data, indent=2)
-        print(string)
 
 class Hep_Parser:
 
+    def __init__(self, source):
 
-    def __init__(self , source):
+        self.List_Of_Results = list()
         self.data = json.loads(source)
 
-    def get_creator_names(self):
-        creator_list = list()
         for dic in self.data:
-            creator_list.append(dic['creator']['full_name'])
-        return creator_list
+            # Handle api error outputing DOI twice
+            if dic['doi'] is None:
+                dic['doi'] = 'DOI not specified'
+            elif isinstance(dic['doi'], list):
+                dic['doi'] = dic['doi'][0]
 
-    def get_title(self):
-        title_list = list()
-        for dic in self.data:
-            title_list.append(dic['title']['title'])
-        return title_list
+            self.List_Of_Results.append([
+                dic['creation_date'],  # 0 DATE
+                dic['creator']['full_name'],  # 1 NAME
+                dic['title']['title'],  # 2 TITLE
+                dic['primary_report_number'],  # 3 ARXIV_ID
+                dic['doi'],  # 4 DIGITAL_OBJECT_ID
+                dic['number_of_citations']])  # 5 CITATIONS
 
-    def get_creation_date(self):
-        date_list = list()
-        for dic in self.data:
-            date_list.append(dic['creation_date'])
-        
-        return date_list
-  
-    def get_citation_count(self):
-        citation_count = list()
-        for dic in self.data:
-            citation_count.append(dic['number_of_citations'])
-        return citation_count
+    def show(self):
+        for List in self.List_Of_Results:
+            print("\n")
+            for el in List:
+                print(el)
+
+    def write(self, filename):
+        with open(filename, 'w') as f:
+            for List in self.List_Of_Results:
+                f.write("\n")
+                for el in List:
+                    f.write(str(el) + "\n")
+
+    def sort_by(self, value):
+        if value == 'name':
+            self.List_Of_Results.sort(key=lambda x: x[1])
+        elif value == 'date':
+            self.List_Of_Results.sort(key=lambda x: x[0])
+        elif value == 'citations':
+            self.List_Of_Results.sort(key=lambda x: x[5], reverse=True)
--- project/pyper.py
+++ project/pyper.py
@@ -2,14 +2,8 @@
 
 import sys
 import argparse
-from hep_classes import Hep_Helper,Hep_Parser
+from hep_classes import Hep_Helper, Hep_Parser
 
-
-def replace_spaces(string):
-    return string.replace(" ", "%20")
-def show_parsed(date,names,titles,citations):
-        for i in range(len(names)):
-            print( "Date: "+ date[i] +"\n" + "Creator: " + names[i] + "\n" + "Title: " + titles[i]+ "\n" + "Citations: "+ str(citations[i])+"\n")
 
 def main():
     commands = ""
@@ -69,42 +63,58 @@ def main():
         required=False,
         help="Specify date range for the query. Examples : -d 2018+ ,-d 2000- ,-d 1980->1982",
         type=str)
-    #Add DOI to the query
+    # Add DOI to the query
     parser_HEP.add_argument(
         "-doi",
         required=False,
         help="Add Digital Object Identifier to the query",
         type=str)
-    #Add journal reference to the query
+    # Add journal reference to the query
     parser_HEP.add_argument(
         "-j",
         required=False,
         help="If you’d like to stay up to date with publications of a specific journal, then a search by journal will be helpful ex: -j Physics.Rev.D",
         type=str)
-    #Add Type-Code to the query
+    # Add Type-Code to the query
     parser_HEP.add_argument(
         "-tc",
         required=False,
-        help="Search for papers of specyfic type ex: b: book ,c: conference paper ,core: work covering high-energy-physics,i: introductory" +\
-             " l: lectures, note: experimental note, p: published,proceedings:collected volume of a conference proceedings, r: review ,t: thesis",
-        type = str,
-        choices = ['b','c','i','l','p','r','t','core','note','proceedings']
-    )
+        help="Search for papers of specyfic type ex: b: book ,c: conference paper ,core: work covering high-energy-physics,i: introductory" +
+        " l: lectures, note: experimental note, p: published,proceedings:collected volume of a conference proceedings, r: review ,t: thesis",
+        type=str,
+        choices=[
+            'b',
+            'c',
+            'i',
+            'l',
+            'p',
+            'r',
+            't',
+            'core',
+            'note',
+            'proceedings'])
 
     parser_HEP.add_argument(
         "-topcite",
         required=False,
         help="If you want to find works that are often cited in other publications, you can use this option to search, ex of use: topcite 100->150",
-        type = str,
+        type=str,
 
     )
-    # Write result to the JSON file
+    # Write result to given output buffer
     parser_HEP.add_argument(
-        "-show",
+        "-out",
         required=False,
-        help="Write contents of the query to the json file",
+        help="Show the query output: cmd->show in terminal, 'filename.txt'->write contents to the file named 'filename.txt'",
         type=str,
-        choices=['json', 'out'],
+    )
+
+    parser_HEP.add_argument(
+        "-sort",
+        required=False,
+        help="Sort by: name, citations or date. Ex: -sort name",
+        type=str,
+        choices=['name', 'citations', 'date']
     )
 
     args = parser.parse_args()
@@ -145,37 +155,43 @@ def main():
         print("Choose at least one query variable try:\n" +
               "python3 pyper.py HEP -h " + "for more information")
     else:
+        # Initial param encoding.
         for i, (el1, el2) in enumerate(filtered_params):
             if(i == len(filtered_params) - 1):
                 commands += el1 + "%3A+" + el2
             else:
                 commands += el1 + "%3A+" + el2 + "%20+"
 
+        # Ensure any possible mistakes by the user are properly encoded.
         commands = hep_helper.hep_url_encode(commands)
-        url = hep_helper.hep_url_generator(
-                commands, hep_args.show)
+        # Generate URL
+        url = hep_helper.hep_url_generator(commands,
+                                           hep_args.out)
+        # Retrieve the data from url
         source = hep_helper.get_source(url)
-        
 
-    if hep_args.show is not None:
-        if hep_args.show == "json":
+        hep_parser = Hep_Parser(source)
 
-            
+    if hep_args.out is None:
+        print("Your output has been written into 'API_OUTPUT_JSON.json'")
 
-            hep_helper.write_to_json(source, "API_OUTPUT_JSON.json")
+    elif hep_args.out == 'cmd':
+        if hep_args.sort is not None:
+            hep_parser.sort_by(hep_args.sort)
 
+            hep_parser.show()
         else:
-            hep_parser = Hep_Parser(source)
+            hep_parser.show()
+    else:
+        if hep_args.sort is not None:
+            hep_parser.sort_by(hep_args.sort)
 
-            creator_names = hep_parser.get_creator_names()
-            titles = hep_parser.get_title()
-            dates = hep_parser.get_creation_date()
-            citations = hep_parser.get_citation_count()
+            hep_parser.write(hep_args.out)
+            print("Your output has been written into " + hep_args.out)
+        else:
+            hep_parser.write(hep_args.out)
+            print("Your output has been written into " + hep_args.out)
 
-            show_parsed(dates,creator_names,titles,citations)
-
-
-        
 
 if __name__ == "__main__":
     main()
